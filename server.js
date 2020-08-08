d19f853cb94c46aa14355931119966aeb9e21fa1
harshith331 <svs.harshith@gmail.com>
2020-08-09 00:16:24 +0530

    commit 0

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+
+// Define Routes
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
